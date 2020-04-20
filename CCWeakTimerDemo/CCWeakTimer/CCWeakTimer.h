3a9669019c2f5cbe139448e47686a5efdc82a2e7
CC (deng you hua) <ccworld1000@gmail.com>
2020-04-20 21:50:32 +0800

    CCAutoTag push : h

---
+++ CCWeakTimerDemo/CCWeakTimer/CCWeakTimer.h
@@ -0,0 +1,17 @@
+//
+//  CCWeakTimer.h
+//  CCWeakTimer
+//
+//  Created by CC on 2020/4/20.
+//  Copyright © 2020 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CCWeakTimer : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CCWeakTimerDemo/CCWeakTimer/CCWeakTimer.m
@@ -0,0 +1,13 @@
+//
+//  CCWeakTimer.m
+//  CCWeakTimer
+//
+//  Created by CC on 2020/4/20.
+//  Copyright © 2020 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+#import "CCWeakTimer.h"
+
+@implementation CCWeakTimer
+
+@end
