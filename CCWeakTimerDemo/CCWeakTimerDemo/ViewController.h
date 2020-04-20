08e998d62f40f2dc46c33f4e6fc0b36ee1b9cf57
CC (deng you hua) <ccworld1000@gmail.com>
2020-04-20 21:38:15 +0800

    CCAutoTag push : 20200420213815

---
+++ CCWeakTimerDemo/CCWeakTimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCWeakTimerDemo
+//
+//  Created by CC on 2020/4/20.
+//  Copyright © 2020 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CCWeakTimerDemo/CCWeakTimerDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CCWeakTimerDemo
+//
+//  Created by CC on 2020/4/20.
+//  Copyright © 2020 CC (deng you hua | cworld1000@gmail.com). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
